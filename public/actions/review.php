ef502f7d27ebb29049337d1053d58266ae3b31b4
erouchota <erouchota@cti.gr>
2022-05-09 14:54:45 +0300

    remove comment

--- public/actions/review.php
+++ public/actions/review.php
@@ -7,7 +7,6 @@ use Hotel\Review;
 
 require_once __DIR__ .'/../../boot/boot.php';
 
-//Return to home page if not a post request
 if (strtolower($_SERVER['REQUEST_METHOD']) != 'post'){
     header('Location: /');
     // header('Location: ./localhost/public/assets/register1.php');
